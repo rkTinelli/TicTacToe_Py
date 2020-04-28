c4de20fff17424927b5668e30ca6e1e4d45a6d67
rkTin <rktinelli@gmail.com>
2020-04-28 23:56:53 +0100

    CheckWin function added

--- TicTacToe_Console.py
+++ TicTacToe_Console.py
@@ -58,5 +58,25 @@ def checkTie(board):
             return True
 
 
+def checkWin(Board,symbol):
+    # Check lines
+    for i in range(3):
+            if board[i][0] == board[i][1] == board[i][2] == symbol:
+                return True
+
+    # Check column
+    for j in range(3):
+            if board[0][j] == board[1][j] == board[2][j] == symbol:
+                return True
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] == symbol:
+        return True
+    if board[0][2] == board[1][1] == board[2][0] == symbol:
+        return True
+
+    return False
+
+
 computerTurn(board)
 showBoard(board)

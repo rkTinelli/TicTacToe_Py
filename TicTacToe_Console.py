73f74186a22c48272cc37e29b5a40535631757a9
rkTin <rktinelli@gmail.com>
2020-04-28 23:24:39 +0100

    Comments and documentation to the functions

--- TicTacToe_Console.py
+++ TicTacToe_Console.py
@@ -7,7 +7,7 @@ board = [
     [" ", " ", " "]
 ]
 
-
+# Display the board for the player in the console
 def showBoard(board):
     for i in range(3):
         for j in range(3):
@@ -18,14 +18,14 @@ def showBoard(board):
         if i != 2:
             print("___________")
 
-
+# Check if the position in the board is empty
 def checkIfEmpty(board, pos):
     if board[pos[0]][pos[1]] != " ":
-        print("Positions occupied, please try again")
         return False
     return True
 
-
+# Simulate the computer turn, verifies if empty, if so, populates it with "X"
+# # Using recursive method will make a loop until a valid position is provided
 def computerTurn(board):
     pc_X = rnd.randrange(0, 3)
     pc_Y = rnd.randrange(0, 3)
@@ -35,7 +35,8 @@ def computerTurn(board):
     else:
         computerTurn(board)
 
-
+# Get the player input, verifies if empty, if so, populates it with "X"
+# Using recursive method will make a loop until a valid position is provided
 def playerTurn(board):
     play_response = (input("Please give the X,Y coordinates of your play: "))
     play_pos = list(map(int, play_response.split(",")))
@@ -43,10 +44,11 @@ def playerTurn(board):
         board[play_pos[0]][play_pos[1]] = "X"
         return True
     else:
+        print("Positions occupied, please try again")
         playerTurn(board)
 
-
-def checkDraw(board):
+# Check if the game resulted in a tie, if there is no spaces available to play
+def checkTie(board):
     # Check all the spaces in the board to
     # find an empty space, if found, return False, else return True
     for i in range(3):
@@ -56,6 +58,5 @@ def checkDraw(board):
             return True
 
 
-playerTurn(board)
 computerTurn(board)
 showBoard(board)
